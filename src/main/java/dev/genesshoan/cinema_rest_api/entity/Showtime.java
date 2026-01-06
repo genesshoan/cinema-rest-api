851a988c2288b5370bea13cbbeb6e8033935cf74
Shoan Genes <shoangenes42@gmail.com>
2026-01-06 15:37:06 -0300

    feat(showtime-entity): Add ShowtimeStatus enum to Showtime

--- src/main/java/dev/genesshoan/cinema_rest_api/entity/Showtime.java
+++ src/main/java/dev/genesshoan/cinema_rest_api/entity/Showtime.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -70,6 +72,13 @@ public class Showtime {
    */
   @Column(name = "base_price", nullable = false)
   private BigDecimal basePrice;
+
+  /**
+   * The status for this showtime
+   */
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  ShowtimeStatus status;
 
   /**
    * Movie associated with this showtime.
---
+++ src/main/java/dev/genesshoan/cinema_rest_api/entity/ShowtimeStatus.java
@@ -0,0 +1,7 @@
+package dev.genesshoan.cinema_rest_api.entity;
+
+public enum ShowtimeStatus {
+  SCHEDULED,
+  COMPLETED,
+  CANCELLED
+}
